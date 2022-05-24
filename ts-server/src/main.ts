d55a7237b2766d60e1734e34f2b1cb515a48327e
Wadii <B00549848@essec.edu>
2022-05-25 01:09:59 +0200

    quick webhook

--- ts-server/src/main.ts
+++ ts-server/src/main.ts
@@ -2,9 +2,15 @@ import * as express from 'express';
 import { TRequest, TResponse } from './main.types';
 const app = express();
 
-const port = 9000;
+const port = 3004;
 
+app.use(express.json())
 app.get('/', (request: TRequest, response: TResponse<{ content: string }>) => {
+    response.status(200).json({ content: 'helloworld' });
+});
+
+app.post('/', (request: TRequest, response: TResponse<{ content: string }>) => {
+    console.log(request.body)
     response.status(200).json({ content: 'helloworld' });
 });
 
